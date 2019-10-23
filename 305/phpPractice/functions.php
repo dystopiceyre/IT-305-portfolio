e6c400ab41b10658e8a9b75962dd7275df9115e3
dystopiceyre <livvy16.olivia@gmail.com>
2019-10-23 09:48:26 -0700

    Updated phpPractice. Added team IDAYDream files to my personal server so I can test the code live.

---
+++ 305/phpPractice/functions.php
@@ -0,0 +1,13 @@
+<?php
+function largest($val1, $val2) {
+    if ($val1 >= $val2) {
+        return $val1;
+    }
+    else {
+        return $val2;
+    }
+}
+
+function average($num1, $num2) {
+    return (($num1 + $num2)/2);
+}
--- 305/phpPractice/phpPractice.php
+++ 305/phpPractice/phpPractice.php
@@ -1,6 +1,26 @@
 <?php
 
+    include "functions.php";
+
+    $lineBreak = '<br>';
     echo "Let's go!";
+    echo $lineBreak;
     print "Let's go!";
 
-?>
+    print $lineBreak;
+    $phrase = ' I like PHP ';
+
+    print $phrase.trim();
+    print $lineBreak;
+    print 'The length of the string is ' .strlen($phrase);
+    print ($lineBreak);
+    print 'The position of the word "PHP" in the string is ' .strpos($phrase, 'PHP');
+    print $lineBreak;
+    print str_replace('like', 'love', $phrase);
+    print $lineBreak;
+    print strtoupper($phrase);
+    print $lineBreak;
+
+    print 'The larger of the two values is' .largest(23, 16);
+    print $lineBreak;
+    print 'The average of 5 and 27 is ' .average(5, 27);
