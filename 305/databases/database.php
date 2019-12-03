31b895035e1a0ba53dc4086bf5b6c9ec6bde5f46
dystopiceyre <livvy16.olivia@gmail.com>
2019-12-02 21:07:29 -0800

    Mass delete of irrelevant files

--- 305/databases/database.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $username = 'oringhis_grcuser';
-    $password = 'grcuserPassword';
-    $hostname = 'localhost';
-    $database = 'oringhis_grc';
-    $cnxn = mysqli_connect($hostname, $username, $password, $database);
